bccfefdc433e64220bd8493c4e5b20862823404d
Marco Romagnolo <marco.romagnolo@igt.com>
2016-08-08 18:16:53 +0200

    Add docs

--- src/test/resources/sql/H2/drop.sql
+++
@@ -1,6 +0,0 @@
---Drop Tables
-
-DROP TABLE IF EXISTS test_brand;
-DROP TABLE IF EXISTS test_category;
-DROP TABLE IF EXISTS test_product_category;
-DROP TABLE IF EXISTS test_product;
--- src/test/resources/sql/SQLITE/drop.sql
+++
@@ -1,6 +0,0 @@
---Drop Tables
-
-DROP TABLE test_brand;
-DROP TABLE test_category;
-DROP TABLE test_product_category;
-DROP TABLE test_product;
