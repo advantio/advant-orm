63dac9cd00e02e9a7357e46e42ee3877476a9b9d
advantio <info@advant.io>
2016-08-08 17:58:29 +0200

    Postgresql fix sequences

--- src/test/resources/sql/POSTGRESQL/create.sql
+++ src/test/resources/sql/POSTGRESQL/create.sql
@@ -5,51 +5,47 @@
 
 --Create Sequences
 
-CREATE SEQUENCE brand_id_seq;
-CREATE SEQUENCE product_id_seq;
-CREATE SEQUENCE category_id_seq;
-CREATE SEQUENCE product_category_id_seq;
+CREATE SEQUENCE brand_id_seq INCREMENT BY 1 START WITH 1;
+CREATE SEQUENCE product_id_seq INCREMENT BY 1 START WITH 1;
+CREATE SEQUENCE category_id_seq INCREMENT BY 1 START WITH 1;
+CREATE SEQUENCE product_category_id_seq INCREMENT BY 1 START WITH 1;
 
 --Create Tables
 
 CREATE TABLE test_brand
 (
-  id bigint NOT NULL DEFAULT nextval('brand_id_seq'::regclass),
+  id bigint NOT NULL DEFAULT nextval('brand_id_seq') PRIMARY KEY,
   version bigint DEFAULT NULL,
-  name character varying(255) NOT NULL,
-  CONSTRAINT pk_brand PRIMARY KEY (id)
+  name character varying(255) NOT NULL
 );
 
 
 CREATE TABLE test_category
 (
-  id bigint NOT NULL DEFAULT nextval('category_id_seq'::regclass),
+  id bigint NOT NULL DEFAULT nextval('category_id_seq') PRIMARY KEY,
   version bigint DEFAULT NULL,
   name character varying(255) NOT NULL,
-  description character varying(1000) NOT NULL,
-  CONSTRAINT pk_category PRIMARY KEY (id)
+  description character varying(1000) NOT NULL
 );
 
 
 CREATE TABLE test_product_category
 (
-  id bigint NOT NULL DEFAULT nextval('product_category_id_seq'::regclass),
+  id bigint NOT NULL DEFAULT nextval('product_category_id_seq') PRIMARY KEY,
   version bigint DEFAULT NULL,
   product_id bigint NOT NULL,
-  category_id bigint NOT NULL,
-  CONSTRAINT pk_product_category PRIMARY KEY (id)
+  category_id bigint NOT NULL
 );
 
 
 CREATE TABLE test_product
 (
-  id bigint NOT NULL DEFAULT nextval('product_id_seq'::regclass),
+  id bigint NOT NULL DEFAULT nextval('product_id_seq') PRIMARY KEY,
   version bigint DEFAULT NULL,
   brand_id integer NOT NULL,
   blocked boolean NOT NULL DEFAULT false,
   name character varying(255) NOT NULL,
   description character varying(255) NOT NULL,
   create_date timestamp without time zone NOT NULL,
-  modify_date timestamp without time zone,
-  CONSTRAINT pk_product PRIMARY KEY (id)
+  modify_date timestamp without time zone
 );
